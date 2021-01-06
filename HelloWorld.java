bc5663122562acdbed6eacda62f76668536d92b1
Ryan Shintaku <ryanshintaku@Ryans-MacBook-Pro.local>
2021-01-06 11:26:06 -0800

    Ryan adding to Coby's HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,3 @@
-System.println("Hello World Coby");
-System.println("I have been here almost 5 years :C");
+System.out.println("Hello World Coby");
+System.out.println("I have been here almost 5 years :C");
+System.out.println("Wassup Coby");
